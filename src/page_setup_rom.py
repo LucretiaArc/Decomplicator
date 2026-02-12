7cbadfb24cb79d21e07d295bc44a7d359e6613ef
LucretiaArc <11419416+LucretiaArc@users.noreply.github.com>
2026-02-12 18:45:06 +1100

    Automatically convert between z64, n64, and v64 formats

--- src/page_setup_rom.py
+++ src/page_setup_rom.py
@@ -71,36 +71,76 @@ class SetupBaseromPage(QWizardPage):
         self.browse_container.setHidden(available)
         self.instruction_text.setText(label_text)
 
+    def check_rom(self, rom_path: pathlib.Path) -> bool:
+        """
+        Check whether the ROM at the given path matches the expected SHA-256 hash, and cache it if it does.
+
+        This method transforms the ROM three times such that, for each group of four bytes ABCD, the bytes will be
+        ordered as BADC, DCBA, and CDAB. Any ROM in z64, n64, or v64 format will be available in all three formats,
+        either as the input ROM, or as one of the transformed ROMs.
+
+        If any transformed version of the ROM is a match, all transformed versions will be cached.
+        :param rom_path: Path to the candidate ROM.
+        :return: True if the ROM matched the expected hash and was cached, False otherwise.
+        """
+
+        byte_order_transforms = (
+            (1, 0, 3, 2),
+            (3, 2, 1, 0),
+            (2, 3, 0, 1),
+        )
+
+        rom_data = bytearray(rom_path.read_bytes())
+        transform_data = [rom_data]
+
+        for byte_order in byte_order_transforms:
+            rom_data_transformed = bytearray(len(rom_data))
+            rom_data_transformed[0::4] = rom_data[byte_order[0]::4]
+            rom_data_transformed[1::4] = rom_data[byte_order[1]::4]
+            rom_data_transformed[2::4] = rom_data[byte_order[2]::4]
+            rom_data_transformed[3::4] = rom_data[byte_order[3]::4]
+            transform_data.append(rom_data_transformed)
+
+        transform_hash = [hashlib.sha256(d).hexdigest() for d in transform_data]
+
+        if self.setup_context.template_config.baserom_hash in transform_hash:
+            for t_data, t_hash in zip(transform_data, transform_hash):
+                f = files.ROM_CACHE_DIR / t_hash
+                f.write_bytes(t_data)
+            return True
+        else:
+            return False
+
     def select_file(self):
         baserom_hash = self.setup_context.template_config.baserom_hash
-        baserom_cache_path = (files.ROM_CACHE_DIR / baserom_hash)
+        baserom_cache_path = files.ROM_CACHE_DIR / baserom_hash
         baserom_cache_path.parent.mkdir(parents=True, exist_ok=True)
         while not baserom_cache_path.exists():
             file_name, _ = QFileDialog.getOpenFileName(
                 self,
                 "Select ROM...",
                 dir=str(files.get_default_directory()),
-                filter="N64 ROM (*.z64)"
+                filter="N64 ROM (*.z64 *.n64 *.v64)"
             )
+
             if not file_name:
                 break
 
             file_path = pathlib.Path(file_name)
             log.info(f"User selected ROM {file_path}")
+
             try:
-                with open(file_path, "rb", buffering=0) as f:
-                    file_hash = hashlib.file_digest(f, "sha256").hexdigest()
+                rom_matches = self.check_rom(file_path)
             except OSError as e:
                 gui_common.error(self, f"Error reading the selected file.\n\n{e.strerror}")
                 continue
 
-            if file_hash == baserom_hash:
-                log.info("ROM hash matches expected hash, caching")
-                file_path.copy(baserom_cache_path)
+            if rom_matches:
+                log.info("ROM hash matched expected hash")
                 self.set_rom_available(True)
                 self.completeChanged.emit()
             else:
                 gui_common.warning(
                     self,
-                    "The selected ROM file doesn't match the required ROM. Please select the correct ROM."
+                    "The selected file doesn't match the required ROM. Please select the correct ROM."
                 )
